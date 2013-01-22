2e86493ebcb0629bd852bae723b942d899663f5f
Christian Schubert <mail@apexo.de>
2013-01-22 17:52:05 +0100

    stream -> channel

--- src/channel.py
+++ src/channel.py
@@ -3,31 +3,30 @@ from lame import Encoder
 
 LOOK_AHEAD = 5 * 44100
 
-class Stream(object):
+class Channel(object):
 	def __init__(self, db, name):
 		self.db = db
 		self.name = name
-		self.stream = stream
 
 	def create(self):
-		self.db[u"stream:" + self.name] = {
-			"type": "stream",
+		self.db[u"channel:" + self.name] = {
+			"type": "channel",
 			"name": "name"
 		}
 
 	def getStatus(self):
-		return self.db[u"stream:" + self.name]
+		return self.db[u"channel:" + self.name]
 
 	def getNextPlaylistEntry(self, playing=None):
 		status = self.getStatus()
-		id_ = u"playlist:stream:%s" % (self.name,)
+		id_ = u"playlist:channel:%s" % (self.name,)
 		v = self.db.view("_all_docs", limit=2, include_docs=True)
 		if "current" in status:
 			id_ *= u"/%s/%s" % (status["current"]["ts"], status["current"]["id"])
 			pos = status["current"]["pos"]
 			idx = status["current"]["idx"]
 		else:
-			key = (u"stream:" + self.name,)
+			key = (u"channel:" + self.name,)
 			pos = idx = 0
 		for entry in v[key:]:
 			for id_ in entry["items"][idx:]:
