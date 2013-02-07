6de6d72f864966b9949029a6c5d506f023ecc0db
Christian Schubert <mail@apexo.de>
2013-02-07 11:03:35 +0100

    ^_^

--- res/design/file/filters/all.js
+++ res/design/file/filters/all.js
@@ -3,7 +3,7 @@ function (doc, req) {
 		return true;
 	}
 	if (doc._id.substring(0, 9) === "playlist:") {
-		return req.query.playlist && doc._id.substring(9, 10 + req.query.playlist.length) === req.query.playlist + "/";
+		return req.query.playlist && doc._id.substring(9, 10 + req.query.playlist.length) === req.query.playlist + ":";
 	}
 	if (doc._id.substring(0, 8) === "channel:") {
 		return req.query.channel && doc._id.substring(8) === req.query.channel;
