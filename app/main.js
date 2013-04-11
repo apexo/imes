3916d6ac2bc59a42c0f66db1633936160cdb045f
Christian Schubert <mail@apexo.de>
2013-04-11 20:10:13 +0200

    b/f
    maybe

--- app/main.js
+++ app/main.js
@@ -180,52 +180,61 @@ function makeLink(data, target, cls, sep, instead) {
 	return n;
 }
 
-function isSpecialArtist(ids, names, isAlbumArtist) {
-	// http://wiki.musicbrainz.org/Special_Purpose_Artist; 2013-04-11
+// http://wiki.musicbrainz.org/Special_Purpose_Artist; 2013-04-11
+SPECIAL_ARTIST_IDS = {
+	"f731ccc4-e22a-43af-a747-64213329e088": "[anonymous]",
+	"33cf029c-63b0-41a0-9855-be2a3665fb3b": "[data]",
+	"314e1c25-dde7-4e4d-b2f4-0a7b9f7c56dc": "[dialogue]",
+	"eec63d3c-3b81-4ad4-b1e4-7c147d4d2b61": "[no artist]",
+	"9be7f096-97ec-4615-8957-8d40b5dcbc41": "[traditional]",
+	"125ec42a-7229-4250-afc5-e057484327fe": "[unknown]"
+}
 
+SPECIAL_ARTIST_NAMES = {
+	"[anonymous]": true,
+	"[data]": true,
+	"[dialogue]": true,
+	"[no artist]": true,
+	"[traditional]": true,
+	"[unknown]": true
+}
+
+// http://wiki.musicbrainz.org/Style/Unknown_and_untitled/Special_purpose_track_title; 2013-04-11
+SPECIAL_TRACK_TITLES = {
+	"[untitled]": true,
+	"[unknown]": true,
+	"[data track]": true
+}
+
+function isSpecialArtist(ids, names, isAlbumArtist) {
 	if (ids && ids.length === 1) {
-		if (false
-			|| isAlbumArtist && ids[0] === "89ad4ac3-39f7-470e-963a-56509c546377" // "Various Artists"
-			|| ids[0] === "f731ccc4-e22a-43af-a747-64213329e088" // "[anonymous]"
-			|| ids[0] === "33cf029c-63b0-41a0-9855-be2a3665fb3b" // "[data]"
-			|| ids[0] === "314e1c25-dde7-4e4d-b2f4-0a7b9f7c56dc" // "[dialogue]"
-			|| ids[0] === "eec63d3c-3b81-4ad4-b1e4-7c147d4d2b61" // "[no artist]"
-			|| ids[0] === "9be7f096-97ec-4615-8957-8d40b5dcbc41" // "[traditional]"
-			|| ids[0] === "125ec42a-7229-4250-afc5-e057484327fe" // "[unknown]"
-		) {
-			return true;
+		if (isAlbumArtist && ids[0] === "89ad4ac3-39f7-470e-963a-56509c546377") {
+			return "Various Artists"
+		}
+		if (SPECIAL_ARTIST_IDS.hasOwnProperty(ids[0])) {
+			return SPECIAL_ARTIST_IDS[ids[0]];
 		}
 	} else if (names && names.length === 1) {
-		if (false
-			|| isAlbumArtist && names[0] === "Various Artists"
-			|| names[0] === "[anonymous]"
-			|| names[0] === "[data]"
-			|| names[0] === "[dialogue]"
-			|| names[0] === "[no artist]"
-			|| names[0] === "[traditional]"
-			|| names[0] === "[unknown]"
-		) {
-			return true;
+		if (isAlbumArtist && names[0] === "Various Artists" || SPECIAL_ARTIST_NAMES.hasOwnProperty(names[0])) {
+			return names[0];
 		}
 	}
-	return false;
+	return null;
 }
 
 function isSpecialTrackTitle(names) {
-	// http://wiki.musicbrainz.org/Style/Unknown_and_untitled/Special_purpose_track_title; 2013-04-11
 	if (names && names.length === 1) {
-		return (false
-			|| names[0] === "[untitled]"
-			|| names[0] === "[unknown]"
-			|| names[0] === "[data track]"
-		);
+		if (SPECIAL_TRACK_TITLES.hasOwnProperty(names[0])) {
+			return names[0];
+		}
 	}
-	return false;
+	return null;
 }
 
 function albumArtistLink(i, target) {
-	if (isSpecialArtist(i.musicbrainz_albumartistid, i.albumartist, true)) {
-		target.appendChild(document.createTextNode(i.albumartist[0] + ": "));
+	var specialname;
+	if (specialname = isSpecialArtist(i.musicbrainz_albumartistid, i.albumartist, true)) {
+		target.appendChild(document.createTextNode(specialname + ": "));
 	} else if (makeLink(i.albumartist, target, "artist-link", ", ")) {
 		target.appendChild(document.createTextNode(": "));
 	}
@@ -235,8 +244,9 @@ function artistLink(i, target, album) {
 	if (album && i.albumartist && JSON.stringify(i.artist) === JSON.stringify(i.albumartist)) {
 		return false;
 	}
-	if (isSpecialArtist(i.musicbrainz_artistid, i.artist, false)) {
-		target.appendChild(document.createTextNode(i.artist[0]));
+	var specialname;
+	if (specialname = isSpecialArtist(i.musicbrainz_artistid, i.artist, false)) {
+		target.appendChild(document.createTextNode(specialname));
 	} else {
 		makeLink(i.artist, target, "artist-link", ", ", "Unknown Artist");
 	}
@@ -248,8 +258,9 @@ function albumLink(i, target) {
 }
 
 function titleLink(i, target) {
-	if (isSpecialTrackTitle(i.title)) {
-		target.appendChild(document.createTextNode(i.title[0]));
+	var specialname;
+	if (specialname = isSpecialTrackTitle(i.title)) {
+		target.appendChild(document.createTextNode(specialname));
 	} else {
 		var instead = i.path.substring(i.path.lastIndexOf("/") + 1);
 		makeLink(i.title, target, "title-link", ", ", "[unknown title: " + instead + "]");
